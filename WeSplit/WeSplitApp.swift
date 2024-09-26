63f0e8fdc2af0938b9817da9fe725d0d7d87d6b5
Jatin Foujdar <jatin321001@gmail.com>
2024-09-26 16:50:02 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by jatin foujdar on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
